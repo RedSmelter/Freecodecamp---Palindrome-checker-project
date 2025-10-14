38a181a2174767a1e7798a632762717bb0fb2ece
RedSmelter <eduardo2001fevereiro@gmail.com>
2025-10-13 22:53:23 -0300

    parei 29

--- step 28.js
+++ step 28.js
@@ -1,4 +1,17 @@
 /*
+Step 28
+You should have seen "freeCodeCamp" printed to the console. This is because .pop() returns the value that was removed from the array - and you pushed "freeCodeCamp" to the end of the array earlier.
 
+But what does .push() return? Assign your existing rows.push() to a new pushed variable, and log it.
 */
 
+let character = 'Hello';
+let count = 8;
+let rows = ["Naomi", "Quincy", "CamperChan"];
+rows.push("freeCodeCamp");
+let pushed = rows.push();
+console.log(pushed);
+
+let popped = rows.pop();
+console.log(popped);
+console.log(rows);
