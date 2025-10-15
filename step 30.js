0e8d8ba605c041b1527c66962f3800a165e5ac28
RedSmelter <eduardo2001fevereiro@gmail.com>
2025-10-15 15:53:18 -0300

    até o 40

--- step 30.js
+++ step 30.js
@@ -1,4 +1,10 @@
 /*
+Step 30
+Change your rows declaration to be assigned an empty array again.
 
+Also, change your 'Hello' string to use double quotes again. Generally, it does not matter which of the two you prefer, but you will want to be consistent in that choice throughout your project.
 */
 
+let character = "Hello";
+let count = 8;
+let rows = [];
--- step 39.js
+++ step 39.js
@@ -1,4 +1,16 @@
 /*
+Step 39
+Unfortunately, now you cannot see what your loop is doing.
 
+Use let to declare a result variable, and assign it an empty string. An empty string is represented by quotation marks with nothing between them, such as "".
 */
 
+const character = "#";
+const count = 8;
+const rows = [];
+
+for (let i = 0; i < count; i = i + 1) {
+  rows.push(i);
+}
+
+let result = "";
